5ff140de05a4139a441ff713918b97d569bbf955
DESKTOP-NLMFL51\ZN <2205567926@qq.com>
2017-06-01 21:15:06 +0800

    no message

--- main/main.js
+++ main/main.js
@@ -1,18 +1,46 @@
-module.exports = function main() {
+module.exports = main
+function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
-    console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+	
+    console.log('o) 最小值 = '+sequence.minimum()+
+'\no) 最大值 = '+sequence.maxmu()+
+'\no) 元素数量 = '+sequence.len+
+'\no) 平均值 = '+sequence.average());
 };
 
 class Sequence {
   constructor(input) {
+      this.len = input.length;
+      this.input = input;
+	  
     // Write your code here
   }
 
   minimum() {
+    var min=this.input[0];
+    for(var i=1;i<this.len;i++) {
+        if(this.input[i]<min)
+        min=this.input[i];
+    }
+    return min;
     // Write your code here
   }
 
+  maxmu(){
+    var max=this.input[0];
+    for(var i=1;i<this.len;i++) {
+        if(this.input[i]>max)
+        max=this.input[i];
+    }
+    return max;
+  }
   // Write your code here
+  
+  average(){
+	  var av=0;
+	  for(var i=0;i<this.len;i++) {
+        av=av+this.input[i];
+    }
+    return (av/this.len).toFixed(2);
+  }
 }
